cd2e749c5f20ef50ebd4dbae0f7426020990365a
Fustran <fustra1@gmail.com>
2018-08-12 23:17:26 -0400

    dynamic jsons and categories, start of mod menu

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let selectedItem;
+let itemsCache = [];
 
 window.onload = () => {
     document.getElementById('search').oninput = () => { searchFilter(); }
@@ -29,6 +30,14 @@ window.onload = () => {
     toggleVids.checked = checkBoolCookie('loadVids', true);
     toggleVids.onchange = (e) => { setCookie('loadVids', e.target.checked, 365); }
     
+    //set up toggleMods switch
+    let toggleMods = document.getElementById('toggleMods');
+    toggleMods.checked = checkBoolCookie('toggleMods', false);
+    toggleMods.onchange = (e) => { 
+        setCookie('toggleMods', e.target.checked, 365);
+        toggleModText(e.target.checked);
+    }
+
     //set up togglePerformance switch
     let togglePerformance = document.getElementById('togglePerformance');
     togglePerformance.checked = checkBoolCookie('togglePerformance', false);
@@ -37,67 +46,83 @@ window.onload = () => {
         togglePerformanceMode(e.target.checked);
     }
     
-    loadItems(['vanilla'], () => {
-        addTextFlair();
-        loadIcons();
-        sortItems(toggleSort.checked);
-        togglePerformanceMode(togglePerformance.checked);
-    });
+    loadItems([
+        'vanilla', 
+        'test'
+    ]);
 }
 
-//function to load item manifests.
-function loadItems(manifests, callback) {
-    if (manifests.length > 0) {
-        let scriptCount = manifests.length;
-        for (let manifest of manifests) {
-            let script = document.createElement('script');
-            script.setAttribute('type', 'text/javascript');
-            script.setAttribute('src', `items/${manifest}_items/itemManifest.js`);
-            script.onload = script.onreadystatechange = () => {
-                script.onload = script.onreadystatechange = null;
-                scriptCount--;
-                if (scriptCount < 1) { callback(script); } 
+//function to reload item manifests and propagate the changes. called when the site starts / loaded mods are changed.
+async function loadItems(manifests) {
+    //make sure we don't load mods that are already loaded
+    for (let setName in itemsCache) {
+        let find = manifests.findIndex(element => element == setName);
+        if (find !== -1) { 
+            manifests.splice(find, 1); 
+        } else {
+            //if something currently loaded is missing from the manifest, unload the set
+            for (let item in itemsCache[setName].items) {
+                let img = document.getElementById(item);
+                img.parentNode.removeChild(img);
             }
-            document.head.appendChild(script);
+            //also delete any empty categories they used
+            for (let category in itemsCache[setName].classInfo) {
+                let catDiv = document.getElementById(category);
+                if (catDiv.childElementCount == 0) { catDiv.parentNode.removeChild(catDiv); }
+            }
+            delete itemsCache[setName];
         }
+    }
+    if (manifests.length > 0) {
+        let items = itemsCache = await loadManifests(manifests);
+        items = addTextFlair(items);
+        createCategories(items);
+        loadIcons(items);
+        sortItems(document.getElementById('toggleSort').checked);
+        togglePerformanceMode(document.getElementById('togglePerformance').checked);
+        toggleModText(document.getElementById('toggleMods').checked);
     }
 }
 
 //iterates through the itemManifest and loads item icons and details from it.
-function loadIcons() {
-    for (let item in items) {
-        let itemClass = items[item].itemClass;
-        let targetCategory = document.getElementsByClassName(itemClass)[0];
-        let itemImg = document.createElement('img');
-        itemImg.classList.add('item');
-        itemImg.src = `static/itemIcons/item_${item}.png`;
-        itemImg.id = item;
-        targetCategory.appendChild(itemImg);
-
-        itemImg.onmouseenter = () => {
-            setInfo(item);
-        }
-
-        itemImg.onmouseleave = (e) => {
-            hideInfo(e);
-        }
-        
-        itemImg.ondragstart = (e) => {
-            if (onMobile()) { 
-                setInfo(item);
-                closeItems(); 
+function loadIcons(items) {
+    for (let setName in items) {
+        let set = items[setName];
+        for (let itemName in set.items) {
+            let item = set.items[itemName];
+            let itemClass = item.itemClass;
+            let targetCategory = document.getElementById(itemClass);
+            let itemImg = document.createElement('img');
+            itemImg.classList.add('item');
+            itemImg.src = `items/${setName}_items/itemIcons/item_${itemName}.png`;
+            itemImg.id = itemName;
+            targetCategory.appendChild(itemImg);
+    
+            itemImg.onmouseenter = () => {
+                setInfo(itemName);
             }
-            changeSelected(e);
-            //return false to prevent dragging 'ghost' effect
-            return false;
-        }
-
-        itemImg.onclick = (e) => {
-            if (onMobile()) { 
-                setInfo(item);
-                closeItems(); 
+    
+            itemImg.onmouseleave = (e) => {
+                hideInfo(e);
             }
-            changeSelected(e);
+            
+            itemImg.ondragstart = (e) => {
+                if (onMobile()) { 
+                    setInfo(itemName);
+                    closeItems(); 
+                }
+                changeSelected(e);
+                //return false to prevent dragging 'ghost' effect
+                return false;
+            }
+    
+            itemImg.onclick = (e) => {
+                if (onMobile()) { 
+                    setInfo(itemName);
+                    closeItems(); 
+                }
+                changeSelected(e);
+            }
         }
     }
 }
@@ -127,7 +152,9 @@ function setInfo(itemName) {
     document.getElementsByClassName('infoImage')[0].innerHTML = '';
     panel.classList.remove('fadeOut');
     panel.classList.add('fadeIn');
-    let item = items[itemName];
+    let result = getItem(itemName);
+    let item = result['item'];
+    let setName = result['setName'];
     
     for (prop in item) {
         //we can get rid of the hasVideo check later once we complete all the mp4s
@@ -169,7 +196,7 @@ function setInfo(itemName) {
     }
     
     let infoImage = document.getElementsByClassName('infoImage')[0];
-    infoImage.src = `static/itemIcons/item_${itemName}.png`;
+    infoImage.src = `items/${setName}_items/itemIcons/item_${itemName}.png`;
     
     document.getElementById('stackTitle').innerHTML = 'stack' in item ? 'Stacking Effect:' : 'Beating Embryo effect:';
     document.getElementById('unlockTitle').innerHTML = 'unlock' in item ? 'Unlock:' : 'Dropped by:';
@@ -181,7 +208,7 @@ function setInfo(itemName) {
     if ('hasVideo' in item && item.hasVideo === true && loadVids.checked === true) {
         video.classList.remove('hidden');
         let source = document.createElement('source');
-        source.setAttribute('src', `static/usageMP4s/usage_${itemName}.mp4`);
+        source.setAttribute('src', `items/${setName}_items/usageMP4s/usage_${itemName}.mp4`);
         video.appendChild(source);
         video.load();
     } else {
@@ -205,13 +232,16 @@ function hideInfo(e) {
 function searchFilter() {
     let text = document.getElementById('search').value;
     let filter = new RegExp(`${text}`, 'i');
-    for (let item in items) {
-        let name = items[item].name;
-        let desc = items[item].description;
-        if (name.match(filter) !== null || desc.match(filter) !== null || item == selectedItem) {
-            document.getElementById(item).classList.remove('faded');
-        } else {
-            document.getElementById(item).classList.add('faded');
+    for (let setName in itemsCache) {
+        let set = itemsCache[setName];
+        for (let item in set.items) {
+            let name = set.items[item].name;
+            let desc = set.items[item].description;
+            if (name.match(filter) !== null || desc.match(filter) !== null || item == selectedItem) {
+                document.getElementById(item).classList.remove('faded');
+            } else {
+                document.getElementById(item).classList.add('faded');
+            }
         }
     }
 
@@ -253,31 +283,95 @@ function togglePerformanceMode(state) {
     }
 }
 
-//opens supplied link in a new tab, instead of replacing the current window like an <a> tag does by default
-function openInTab(href) {
-    let newWindow = window.open(href, '_blank');
-    newWindow.focus();
+//function to toggle the mod menu on or off.
+function toggleModText(state) {
+    if (state && state === true) {
+        document.getElementById('modWrap').classList.remove('hidden');
+    } else {
+        document.getElementById('modWrap').classList.add('hidden');
+    }
 }
 
 //sorts based on toggleSort's status by using the order property. false is sort alphabetically, true is by the commandSort list.
 function sortItems(toggleSort) {
+    let commandSort = [];
+    for (setName in itemsCache) {
+        let set = itemsCache[setName];
+        commandSort = commandSort.concat(set.commandSort);
+    }
     for (i=0; i < commandSort.length; i++) {
         document.getElementById(commandSort[i]).style = toggleSort === true ? `order: ${i};` : '';
     }
 }
 
 //adds extra visual flair to item descriptions. We use this so we don't clog up our descriptions with HTML tags
-function addTextFlair() {
-    for (let i in items) {
-        if (items[i].name) {
-            //add artifact label to artifacts
-            if (items[i].itemClass == 'purple') {
-                items[i].description = `<span class='purple'>ARTIFACT.</span><br><span class='gray'>This item must be toggled on the character select screen.</span><br><br>${items[i].description}`;
+function addTextFlair(items) {
+    for (let setName in items) {
+        let set = items[setName];
+        for (let itemName in set.items) {
+            if (set.items[itemName].name) {
+                //add artifact label to artifacts
+                if (set.items[itemName].itemClass == 'purple') {
+                    items[setName].items[itemName].description = `<span class='purple'>ARTIFACT.</span><br><span class='gray'>This item must be toggled on the character select screen.</span><br><br>${items[setName].items[itemName].description}`;
+                }
+                //add color tags to keywords
+                items[setName].items[itemName].description = items[setName].items[itemName].description.replace(/On hit:/i, '<span class=\'red\'>On hit:</span>');
+                items[setName].items[itemName].description = items[setName].items[itemName].description.replace(/On kill:/i, '<span class=\'red\'>On kill:</span>');
+                items[setName].items[itemName].description = items[setName].items[itemName].description.replace(/On crit:/i, '<span class=\'red\'>On crit:</span>');
             }
-            //add color tags to keywords
-            items[i].description = items[i].description.replace(/On hit:/i, '<span class=\'red\'>On hit:</span>');
-            items[i].description = items[i].description.replace(/On kill:/i, '<span class=\'red\'>On kill:</span>');
-            items[i].description = items[i].description.replace(/On crit:/i, '<span class=\'red\'>On crit:</span>');
         }
     }
+    return items;
+}
+
+//function to generate categories based on what sets are being loaded.
+function createCategories(items) {
+    let categories = {};
+    //make sure we don't have duplicate group names or colors
+    for (let setName in items) {
+        let set = items[setName];
+        for (let category in set.classInfo) {
+            if (!alreadyExists(category, set.classInfo[category], categories)) {
+                categories[category] = set.classInfo[category];
+            }
+        }
+    }
+    //create the categories
+    for (let catName in categories) {
+        let itemPanel = document.getElementById('itemPanel');
+        let catDiv = document.createElement('div');
+        catDiv.id = catName;
+        catDiv.classList.add('category');
+        catDiv.style = `color:${categories[catName]};border-color:${categories[catName]};`
+        itemPanel.appendChild(catDiv);
+    }
+}
+
+//retrieves an item object and set from the cache by name
+function getItem(target) {
+    for (let setName in itemsCache) {
+        let set = itemsCache[setName];
+        for (let itemName in set.items) {
+            if (target == itemName) {
+                return {"item": set.items[itemName], "setName": setName};
+            }
+        }
+    }
+    return null;
+}
+
+//function to check for a key/value match in an obj.
+function alreadyExists(key, value, obj) {
+    for (let objKey in obj) {
+        if (objKey == key || obj[objKey] == value) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//opens supplied link in a new tab, instead of replacing the current window like an <a> tag does by default
+function openInTab(href) {
+    let newWindow = window.open(href, '_blank');
+    newWindow.focus();
 }
